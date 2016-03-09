d13f78825769d36b7d17d554453f57c285b1fca2
Cameron Stanley <cameronstanley23@gmail.com>
2016-03-09 09:03:32 -0700

    Basic playlist RSS feed builder implemented

--- app/services/playlist_rss_feed_builder.rb
+++ app/services/playlist_rss_feed_builder.rb
@@ -2,13 +2,22 @@ class PlaylistRssFeedBuilder
 
   def initialize(rss_feed)
     @rss_feed = rss_feed
-    @playlist = RSpotify::Playlist.find(rss_feed.user.spotify_id, rss_feed.spotify_playlist_id)
+    @spotify_user = RSpotify::User.new(rss_feed.user.spotify_user)
+    @playlist = @spotify_user.playlists.select {|playlist| playlist.id == rss_feed.spotify_playlist_id}.first
   end
 
   def build_from_rss_feed
     feed = Feedjira::Feed.fetch_and_parse(@rss_feed.url)
     feed.entries.each do |entry|
       search_query = entry.title.gsub(/\:/, '')
+      found_tracks = RSpotify::Track.search(search_query)
+      
+      if found_tracks.any?
+        track = found_tracks.first
+
+        @playlist.add_tracks!([track])
+        puts "Adding track #{track.name} to #{@playlist.name} playlist."
+      end
     end
   end
 
--- lib/tasks/playlist.rake
+++ lib/tasks/playlist.rake
@@ -1,9 +1,14 @@
 namespace :playlist do
 
-  desc 'Builds playlists using tracks from RSS feeds'
+  desc 'Builds playlists using tracks parsed from RSS feeds'
   task build_from_rss_feeds: :environment do
     RssFeed.all.each do |rss_feed|
-      PlaylistRssFeedBuilder.new(rss_feed).build_from_rss_feed
+      begin
+        PlaylistRssFeedBuilder.new(rss_feed).build_from_rss_feed
+      rescue Exception => e
+        puts "Error building playlist for RSS Feed id=#{rss_feed.id}: #{e.message}"
+        puts e.backtrace
+      end
     end
   end
 
