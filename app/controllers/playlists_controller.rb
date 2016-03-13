6eea8f65025e46b0c6a10e415860d0155131a0c6
Cameron Stanley <cameronstanley23@gmail.com>
2016-03-13 08:50:21 -0700

    Implement pagination for playlists#index

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -3,9 +3,9 @@ class PlaylistsController < ApplicationController
   before_action :get_playlist, only: [:show, :export_as_csv]
 
   def index
-    @playlists = @spotify_user.playlists    
+    get_paginated_playlists
   end
-
+    
   def show
     current_page = params[:page].to_i || 0
     tracks = @playlist.tracks(limit: 100, offset: current_page * 100)
@@ -23,6 +23,23 @@ class PlaylistsController < ApplicationController
 
   def get_playlist
     @playlist = RSpotify::Playlist.find(params[:user_id], params[:id])
+  end
+
+  def get_paginated_playlists
+    current_page = params[:page].to_i || 0
+
+    begin
+      RSpotify.raw_response = true
+      playlists_response = JSON.parse(@spotify_user.playlists(limit: 20, offset: current_page * 20))
+      playlists = playlists_response["items"].map { |i| RSpotify::Playlist.new(i) } 
+      playlists_total = playlists_response["total"]
+      @playlists = WillPaginate::Collection.create(current_page + 1, 20, playlists_total) do |pager|
+        pager.replace(playlists)
+      end 
+    rescue Exception => e
+    end
+
+    RSpotify.raw_response = false
   end
 
 end
