563612b24512421a16e4c12aa46739b6436e7d5b
Cameron Stanley <cameronstanley23@gmail.com>
2016-03-22 22:22:24 -0500

    Render flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+  def alert_container_class(flash_type)
+    {
+      success: "alert-success", 
+      error: "alert-danger", 
+      alert: "alert-warning", 
+      notice: "alert-info"
+    }[flash_type.to_sym] || flash_type.to_s
+  end
+
+  def alert_icon_class(flash_type)
+    {
+      success: "glyphicon glyphicon-ok",
+      error: "glyphicon glyphicon-remove",
+      alert: "glyphicon glyphicon-warning-sign",
+      notice: "glyphicon glyphicon-info-sign"
+    }[flash_type.to_sym] || flash_type.to_s
+  end
+
 end
